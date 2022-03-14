4a3bc4d30a54f244e8d4cb5fc0743f691e175753
Beau Grimmel <beaugrimmel@gmail.com>
2022-03-14 15:10:22 -0400

    Many changes

--- Game.js
+++ Game.js
@@ -1,39 +1,71 @@
-startGame();
-const correctAns = generateFretToGuess();
-console.log("Correct answer is " + correctAns);
+export default class Game {
+  constructor() {
+    this.correctAns = null;
+    this.numCorrect = 0;
+    this.numGuesses = 0;
+    document.addEventListener("click", this.handleMouseClick);
+  }
 
-function startGame() {
-  document.addEventListener("click", handleMouseClick);
-}
+  handleMouseClick = (event) => {
+    if (event.target.matches("[data-note]")) {
+      this.pressNote(event);
+      return;
+    }
 
-function stopGame() {
-  document.removeEventListener("click", handleMouseClick);
-}
+    // TODO - handle other possible mouse clicks
+  };
 
-function handleMouseClick(e) {
-  if (e.target.matches("[data-note]")) {
-    console.log("You chose " + e.target.dataset.note);
-    pressNote(e.target.dataset.key);
+  pressNote(event) {
+    // TODO
+    var note = event.target.dataset.note;
+    if (note == this.correctAns) {
+      this.numCorrect += 1;
+      this.resetNoteSelector();
+      this.resetFretboard();
+      this.generateFretToGuess();
+      console.log("Hey cheater... the correct answer is " + this.correctAns);
+    } else {
+      console.log("Wrong, thats a " + note);
+      event.target.classList.add("wrong");
+    }
+    this.numGuesses += 1;
     return;
   }
 
-  // TODO - handle other possible mouse clicks
-}
+  generateFretToGuess() {
+    const fretboardGrid = document.querySelector("[data-fretboard-grid]");
+    const frets = fretboardGrid.querySelectorAll("[data-note]");
 
-function pressNote(e) {
-  // TODO
-  return;
-}
-
-function generateFretToGuess() {
-  const fretboardGrid = document.querySelector("[data-fretboard-grid]");
-  const frets = fretboardGrid.querySelectorAll("[data-note]");
-
-  var select = Math.floor(Math.random() * frets.length);
-  for (var i = 0; i < frets.length; ++i) {
-    if (i == select) {
-      frets[i].className = "fret selected";
-      return frets[i].dataset.note;
+    var select = Math.floor(Math.random() * frets.length);
+    for (var i = 0; i < frets.length; ++i) {
+      if (i == select) {
+        frets[i].classList.add("selected");
+        this.correctAns = frets[i].dataset.note;
+      }
     }
   }
+
+  resetFretboard() {
+    const fretboardGrid = document.querySelector("[data-fretboard-grid]");
+    const frets = fretboardGrid.querySelectorAll("[data-note]");
+
+    for (var i = 0; i < frets.length; ++i) {
+      frets[i].classList.remove("selected");
+    }
+    return;
+  }
+
+  resetNoteSelector() {
+    const noteSelector = document.querySelector("[data-note-selector]");
+    const notes = noteSelector.querySelectorAll("[data-note]");
+
+    for (var i = 0; i < notes.length; ++i) {
+      notes[i].classList.remove("wrong");
+    }
+    return;
+  }
 }
+
+var game = new Game();
+game.generateFretToGuess();
+console.log("Hey cheater... the correct answer is " + game.correctAns);
